cf3577edfb6e80961f6b9fe2aaff33ffb20a098b
jaimin0609 <42791730+jaimin0609@users.noreply.github.com>
2023-03-17 14:55:17 +0800

    all files

--- subscribe.php
+++ subscribe.php
@@ -1,11 +1,17 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-  if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    // Save the email address to your database or email marketing service
-    echo "Thank you for subscribing!";
-  } else {
-    echo "Invalid email address";
-  }
+if(isset($_POST['submit'])){
+   $to = "jaimin06091995@gmail.com"; // Enter your email address here
+   $from = $_POST['email']; // This will be the user's email address
+   $subject = "Newsletter subscription request";
+   $message = "Please add me to your newsletter list.";
+   $headers = "From:" . $from;
+   mail($to,$subject,$message,$headers);
+   echo "Thank you for subscribing to our newsletter!";
 }
 ?>
+
+<form action="" method="post">
+   <label for="email">Email Address:</label>
+   <input type="email" id="email" name="email" required>
+   <input type="submit" name="submit" value="Subscribe">
+</form>
